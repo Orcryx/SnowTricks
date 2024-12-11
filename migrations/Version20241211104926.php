5eb47fa8ff1cb09238d65b78801dfe635632e496
Orcryx <lauryannmagar@gmail.com>
2024-12-11 11:50:47 +0100

    Creation de la BDD DBeaver via Doctrine

--- migrations/Version20241211104926.php
+++ migrations/Version20241211104926.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241211103849 extends AbstractMigration
+final class Version20241211104926 extends AbstractMigration
 {
     public function getDescription(): string
     {
